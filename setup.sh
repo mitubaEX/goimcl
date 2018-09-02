2c730c9927708ecc2615610007260d0898d9f4d4
mitubaEX <g1344955@cse.kyoto-su.ac.jp>
2018-09-03 02:00:21 +0900

    add twitter account

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ export ACCESS_TOKEN_SECRET=<Your Token>
 
 mkdir Images
 
-for userID in Story_terrorV2 satake_take
+for userID in Story_terrorV2 satake_take WYS_ hikol
 do
   python3 imgcrawler.py "$userID"
 done
